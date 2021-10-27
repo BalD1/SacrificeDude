45cededdbdc611fdb4998dfac3b1f5914569bc5c
Florian <florianhusson@hotmail.fr>
2021-10-27 21:36:31 +0200

    pumpkin_start

---
+++ spooktober2021/Assets/Scripts/Characters/Enemies/Pumpkin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pumpkin : Enemy
+{
+    [Header("Pumpkin related")]
+    [SerializeField] private PumpkinEventsHandler eventsHandler;
+
+    private void Start()
+    {
+        CallStart();
+
+        eventsHandler._OnAttackEvent += LaunchFireball;
+
+        canAttack = true;
+        ai.maxSpeed = stats.speed;
+        ai.endReachedDistance = stopDistance;
+        _Death += OnDeathEvent;
+        EnemyState = EnemyStates.Moving;
+
+        animator.SetTrigger("attack");
+    }
+
+    public void LaunchFireball()
+    {
+        Debug.Log("yo");
+    }
+}
---
+++ spooktober2021/Assets/Scripts/Characters/Enemies/PumpkinEventsHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PumpkinEventsHandler : MonoBehaviour
+{
+    public delegate void OnAttackEvent();
+    public OnAttackEvent _OnAttackEvent;
+
+    public void LaunchFireball()
+    {
+        _OnAttackEvent();
+    }
+}
