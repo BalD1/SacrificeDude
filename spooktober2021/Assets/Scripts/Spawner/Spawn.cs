f087a3ba0a4ef52250a3db11ded6612a0de32be2
Florian <florianhusson@hotmail.fr>
2021-10-29 00:44:48 +0200

    spawneffect + bugfixes

--- spooktober2021/Assets/Scripts/Spawner/Spawn.cs
+++ spooktober2021/Assets/Scripts/Spawner/Spawn.cs
@@ -10,6 +10,7 @@ public class Spawn : MonoBehaviour
         public string enemyName;
         public GameObject enemy;
         public Transform point;
+        public GameObject spawnEffect;
     }
     [SerializeField] private List<SpawnPointData> waveData;
     public List<SpawnPointData> WaveData
@@ -20,6 +21,16 @@ public class Spawn : MonoBehaviour
     private void OnEnable()
     {
         foreach(SpawnPointData data in waveData)
+        {
+            Instantiate(data.spawnEffect, data.point.position, Quaternion.identity);
+        }
+        StartCoroutine(WaitForEffect(1.5f));
+    }
+
+    private IEnumerator WaitForEffect(float time)
+    {
+        yield return new WaitForSeconds(time);
+        foreach (SpawnPointData data in waveData)
         {
             Enemy enemy = Instantiate(data.enemy, data.point.position, Quaternion.identity).GetComponentInChildren<Enemy>();
             enemy.SetEnemyTarget(GameManager.Instance.Player);
